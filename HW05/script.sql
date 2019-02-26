98a22d6aef9a6d9a7e146801231303475096f2f9
tikskit <tikskit@gmail.com>
2019-02-26 19:26:15 +0700

    1. Исправления по домашке 5 2. Домашка 6

--- HW05/script.sql
+++ HW05/script.sql
@@ -1,39 +1,38 @@
-﻿-- 1. Посчитать среднюю цену товара
-
-SELECT AVG(UnitPrice)
-FROM Warehouse.StockItems
-
--- общую сумму продажи по месяцам
-SELECT DATEPART(MONTH, Invoices.InvoiceDate), SUM(InvoiceLines.UnitPrice * InvoiceLines.Quantity)
+﻿-- 1. Посчитать среднюю цену товара, общую сумму продажи по месяцам
+SELECT MONTH(Invoices.InvoiceDate) [Номер месяца]
+	, AVG(InvoiceLines.UnitPrice) [Средняя цена товара]
+	, SUM(InvoiceLines.UnitPrice * InvoiceLines.Quantity) [Общая сумма продажи по месяцам]
 FROM Sales.InvoiceLines 
 INNER JOIN Sales.Invoices ON Invoices.InvoiceID = InvoiceLines.InvoiceID
-GROUP BY DATEPART(MONTH, Invoices.InvoiceDate)
+GROUP BY MONTH(Invoices.InvoiceDate)
+ORDER BY MONTH(Invoices.InvoiceDate)
 
 
 -- 2. Отобразить все месяцы, где общая сумма продаж превысила 10 000 
-SELECT DATEPART(MONTH, Invoices.InvoiceDate)
+SELECT MONTH(Invoices.InvoiceDate)
 FROM Sales.InvoiceLines 
 INNER JOIN Sales.Invoices ON Invoices.InvoiceID = InvoiceLines.InvoiceID
-GROUP BY DATEPART(MONTH, Invoices.InvoiceDate)
+GROUP BY MONTH(Invoices.InvoiceDate)
 HAVING SUM(InvoiceLines.Quantity * InvoiceLines.UnitPrice) > 10000
+ORDER BY MONTH(Invoices.InvoiceDate)
 
 
 -- 3. Вывести сумму продаж, дату первой продажи и количество проданного 
 --по месяцам, по товарам, продажи которых менее 50 ед в месяц.
-SELECT DATEPART(MONTH, Invoices.InvoiceDate) AS [MonthNo]
+SELECT MONTH(Invoices.InvoiceDate) AS [MonthNo]
 	, InvoiceLines.StockItemID
 	, SUM(InvoiceLines.Quantity * InvoiceLines.UnitPrice) AS [Сумма продаж]
 	, MIN(Invoices.InvoiceDate) AS [Дата первой продажи]
 	, SUM (InvoiceLines.Quantity) AS [Количество проданного]
-	, CASE WHEN GROUPING(DATEPART(MONTH, Invoices.InvoiceDate)) = 1 THEN 'Overall'
+	, CASE WHEN GROUPING(MONTH(Invoices.InvoiceDate)) = 1 THEN 'Overall'
 		WHEN GROUPING(InvoiceLines.StockItemID) = 1 THEN 'In the month'
 		ELSE 'For the item in the month'
 	END AS [Note]
 FROM Sales.InvoiceLines
 INNER JOIN Sales.Invoices ON Invoices.InvoiceID=InvoiceLines.InvoiceID
-GROUP BY ROLLUP (DATEPART(MONTH, Invoices.InvoiceDate), InvoiceLines.StockItemID)
-HAVING (SUM(InvoiceLines.Quantity) < 500) 
-	OR (GROUPING(DATEPART(MONTH, Invoices.InvoiceDate)) = 1 
+GROUP BY ROLLUP (MONTH(Invoices.InvoiceDate), InvoiceLines.StockItemID)
+HAVING (SUM(InvoiceLines.Quantity) < 50) 
+	OR (GROUPING(MONTH(Invoices.InvoiceDate)) = 1 
 	OR GROUPING(InvoiceLines.StockItemID) = 1)
-ORDER BY 1, 2
+ORDER BY [MonthNo], InvoiceLines.StockItemID
 
---
+++ HW06/script.sql
@@ -0,0 +1,148 @@
+﻿/*
+1.Сделать расчет суммы продаж нарастающим итогом по месяцам с 2015 года (в рамках одного месяца он будет одинаковый, нарастать будет в течение 
+времени выборки)
+
+Выведите id продажи, название клиента, дату продажи, сумму продажи, сумму нарастающим итогом
+*/
+SELECT
+	Invoices.InvoiceID
+	, Customers.CustomerName
+	, Invoices.InvoiceDate
+	, SUM(InvoiceLines.Quantity * InvoiceLines.UnitPrice) OVER (PARTITION BY Invoices.InvoiceID) [Сумма продажи]
+	, SUM(InvoiceLines.Quantity * InvoiceLines.UnitPrice) 
+		OVER(ORDER BY DATEPART(YEAR, Invoices.InvoiceDate), DATEPART(MONTH, Invoices.InvoiceDate)) [Сумма нарастающим итогом]
+FROM Sales.Invoices
+INNER JOIN Sales.Customers ON Customers.CustomerID=Invoices.CustomerID
+INNER JOIN Sales.InvoiceLines ON InvoiceLines.InvoiceID=Invoices.InvoiceID
+WHERE InvoiceDate >= '20150101'
+ORDER BY Invoices.InvoiceDate, Invoices.CUstomerID
+
+
+-- Сделать 2 варианта запроса - через windows function и без них.
+
+-- То же самое без оконных функций
+
+SELECT
+	I.InvoiceID
+	, Customers.CustomerName
+	, I.InvoiceDate
+	, (
+		SELECT SUM(IL.Quantity * IL.UnitPrice) 
+		FROM Sales.InvoiceLines AS IL 
+		WHERE IL.InvoiceID=I.InvoiceID
+	) AS [Сумма продажи]
+	, (
+		SELECT SUM(IL.Quantity * IL.UnitPrice) 
+		FROM Sales.InvoiceLines AS IL 
+		INNER JOIN Sales.Invoices AS IInr ON IInr.InvoiceID=IL.InvoiceID
+		WHERE IInr.InvoiceDate <= EOMONTH(I.InvoiceDate)
+			AND IInr.InvoiceDate >= '20150101'
+	) [Сумма нарастающим итогом]
+FROM Sales.Invoices AS I
+INNER JOIN Sales.Customers ON Customers.CustomerID=I.CustomerID
+INNER JOIN Sales.InvoiceLines ON InvoiceLines.InvoiceID=I.InvoiceID
+WHERE InvoiceDate >= '20150101'
+ORDER BY I.InvoiceDate, I.CUstomerID
+
+
+
+--Написать какой быстрее выполняется, сравнить по set statistics time on;
+/*
+Время выполнение первого запроса:
+SQL Server parse and compile time: 
+   CPU time = 0 ms, elapsed time = 0 ms.
+
+ SQL Server Execution Times:
+   CPU time = 0 ms,  elapsed time = 0 ms.
+SQL Server parse and compile time: 
+   CPU time = 0 ms, elapsed time = 0 ms.
+
+Второго:
+ SQL Server Execution Times:
+   CPU time = 125 ms,  elapsed time = 1145 ms.
+*/
+
+
+-- 2. Вывести список 2х самых популярных продуктов (по кол-ву проданных) в каждом месяце за 2016й год (по 2 самых популярных продукта в каждом месяце)
+
+; WITH ItemPerMonthCount(MonthNo, StockItemID, Cnt) AS
+(
+	SELECT MONTH(Invoices.InvoiceDate), InvoiceLines.StockItemID, COUNT(*)
+	FROM Sales.InvoiceLines
+	INNER JOIN Sales.Invoices ON Invoices.InvoiceID=InvoiceLines.InvoiceID
+	WHERE YEAR(Invoices.InvoiceDate) = 2016
+	GROUP BY MONTH(Invoices.InvoiceDate), InvoiceLines.StockItemID
+), 
+RangedSales(StockItemID, MonthNo, RN) AS
+(
+	SELECT ItemPerMonthCount.StockItemID
+		, ItemPerMonthCount.MonthNo
+		, ROW_NUMBER() OVER(PARTITION BY ItemPerMonthCount.MonthNo ORDER BY Cnt DESC) AS RN
+	FROM ItemPerMonthCount
+)
+SELECT RangedSales.MonthNo, StockItems.StockItemName
+FROM RangedSales
+INNER JOIN Warehouse.StockItems ON StockItems.StockItemID=RangedSales.StockItemID
+WHERE RangedSales.RN <= 2
+
+
+/*
+3. Функции одним запросом
+Посчитайте по таблице товаров, в вывод также должен попасть ид товара, название, брэнд и цена
+пронумеруйте записи по названию товара, так чтобы при изменении буквы алфавита нумерация начиналась заново
+посчитайте общее количество товаров и выведете полем в этом же запросе
+посчитайте общее количество товаров в зависимости от первой буквы названия товара
+отобразите следующий id товара исходя из того, что порядок отображения товаров по имени 
+предыдущий ид товара с тем же порядком отображения (по имени)
+названия товара 2 строки назад, в случае если предыдущей строки нет нужно вывести "No items"
+сформируйте 30 групп товаров по полю вес товара на 1 шт
+Для этой задачи НЕ нужно писать аналог без аналитических функций
+*/
+
+
+SELECT StockItemID, StockItemName, Brand
+	, ROW_NUMBER() OVER(PARTITION BY LEFT(StockItemName, 1) ORDER BY StockItemName) [Нумерация с группировкой по первой букве]
+	, COUNT(*) OVER() [Общее количество товаров]
+	, COUNT(*) OVER(PARTITION BY LEFT(StockItemName, 1)) [Количество в звисимости от первой буквы]
+	, LEAD(StockItemID) OVER(ORDER BY StockItemName) [ID следующей строки при сортировке по StockItemName]
+	, LAG(StockItemID) OVER(ORDER BY StockItemName) [ID предыдущей строки при сортировке по StockItemName]
+	, ISNULL(CAST(LAG(StockItemID, 2) OVER(ORDER BY StockItemName) AS VARCHAR(10)), 'No items') [ID 2 строки назад]
+	, NTILE(30) OVER(ORDER BY TypicalWeightPerUnit) [TypicalWeightPerUnitGroups]
+FROM Warehouse.StockItems
+
+
+
+--4. По каждому сотруднику выведите последнего клиента, которому сотрудник что-то продал
+
+SELECT People.FullName, Customers.CustomerName
+FROM (
+	SELECT CustomerTransactions.CustomerID, Invoices.SalespersonPersonID, TransactionDate
+		, ROW_NUMBER() OVER(PARTITION BY SalespersonPersonID ORDER BY TransactionDate DESC) AS RN
+	
+	FROM Sales.CustomerTransactions 
+	INNER JOIN Sales.Invoices ON Invoices.InvoiceID=CustomerTransactions.InvoiceID
+) AS R
+INNER JOIN Application.People ON R.SalespersonPersonID=People.PersonID
+INNER JOIN Sales.Customers ON Customers.CustomerID=R.CustomerID
+WHERE R.RN=1
+
+
+/*
+5. Выберите по каждому клиенту 2 самых дорогих товара, которые он покупал
+В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки
+*/
+
+/*Оставлю в таком виде, но в условии задания есть неоднозначность. Клиент мог купить один и тот же товар несколько
+раз. По идее, такой товар нужно отобразить в выборке только 1 раз, потому что нужно выбрать 2 самых дорогих, а это значит, что все товары должны быть 
+разные. Но тогда не понятно какую из дат покупки отображать */
+
+SELECT Customers.CustomerID, Customers.CustomerName, R.StockItemID, R.StockItemName, R.UnitPrice, R.InvoiceDate, R.RN
+FROM (
+	SELECT Invoices.CustomerID, InvoiceLines.StockItemID, StockItems.StockItemName, StockItems.UnitPrice, Invoices.InvoiceDate
+		, ROW_NUMBER() OVER (PARTITION BY Invoices.CustomerID ORDER BY StockItems.UnitPrice DESC) AS RN
+	FROM Sales.InvoiceLines
+	INNER JOIN Sales.Invoices ON Invoices.InvoiceID=InvoiceLines.InvoiceLineID
+	INNER JOIN Warehouse.StockItems ON InvoiceLines.StockItemID=StockItems.StockItemID
+) AS R
+INNER JOIN Sales.Customers ON Customers.CustomerID=R.CustomerID
+WHERE R.RN <= 2
