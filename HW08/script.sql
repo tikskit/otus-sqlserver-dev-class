909276f28a5bf9169cbab29a13832d2b99d1a512
tikskit <tikskit@gmail.com>
2019-03-07 10:05:23 +0700

    Исправления в домашку 8

--- HW08/script.sql
+++ HW08/script.sql
@@ -28,7 +28,7 @@ FROM (
 	WHERE C.CustomerID BETWEEN 2 AND 6
 ) AS D
 PIVOT(COUNT([InvoiceID]) FOR D.SpecName IN([Sylvanite, MT], [Peeples Valley, AZ], [Medicine Lodge, KS], [Gasport, NY], [Jessie, ND]) ) AS P
-
+ORDER BY CAST(InvoiceMonth AS DATE)
 
 
 /*
@@ -38,14 +38,14 @@ PIVOT(COUNT([InvoiceID]) FOR D.SpecName IN([Sylvanite, MT], [Peeples Valley, AZ]
 
 SELECT SpecName, AddrSrc, Addr
 FROM (
-	SELECT SName.SpecName, DeliveryAddressLine1, DeliveryAddressLine2
+	SELECT SName.SpecName, DeliveryAddressLine1, DeliveryAddressLine2, PostalAddressLine1, PostalAddressLine2
 	FROM Sales.Customers AS C
 	CROSS APPLY (SELECT FirstBracketPos=CHARINDEX('(', C.CustomerName)) AS FBP
 	CROSS APPLY (SELECT LastBracketPos=CHARINDEX(')', C.CustomerName, FirstBracketPos + 1)) AS LBP
 	CROSS APPLY (SELECT SpecName=SUBSTRING(C.CustomerName, FirstBracketPos + 1, LastBracketPos - FirstBracketPos - 1)) AS SName
 	WHERE C.CustomerID BETWEEN 2 AND 6
 ) AS S
-UNPIVOT(Addr FOR AddrSrc IN (DeliveryAddressLine1, DeliveryAddressLine2)) AS U
+UNPIVOT(Addr FOR AddrSrc IN (DeliveryAddressLine1, DeliveryAddressLine2, PostalAddressLine1, PostalAddressLine2)) AS U
 
 
 /*
