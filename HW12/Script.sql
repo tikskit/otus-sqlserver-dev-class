ddc69bcd67e69f8569bbc22d0b1c409d34ccc9c9
tikskit <tikskit@gmail.com>
2019-04-17 18:03:21 +0700

    Исправления к домашке #12

--- HW12/Script.sql
+++ HW12/Script.sql
@@ -1,14 +1,13 @@
 ﻿-- 1) Написать функцию возвращающую Клиента с набольшей суммой покупки.
 
 IF OBJECT_ID('Sales.GetBestBuyer') IS NOT NULL
-	DROP PROCEDURE Sales.GetBestBuyer;
+	DROP FUNCTION Sales.GetBestBuyer;
 GO
 
-CREATE PROCEDURE Sales.GetBestBuyer
-	@CustomerID INT OUTPUT
+CREATE FUNCTION Sales.GetBestBuyer()
+RETURNS INT
 AS
 BEGIN
-	SET NOCOUNT ON;	
 	/*В этой ХП подойдет уровень изоляции транзакций READ COMMITED, потому что требуются подтвержденные данные, 
 	ввод которых не был по каким-то причинам отменён. Кроме того, эти данные требуются в согласованном
 	состоянии. А значит READ UNCOMMITED не подойдет. 
@@ -23,6 +22,7 @@ BEGIN
 	во всех приведенных тут ХП и функциях используется READ COMMITED по тем же самым причинам
 	
 	*/
+	DECLARE @CustometID INT
 
 	; WITH PurchaseSum AS (
 		SELECT InvoiceLines.InvoiceID, SUM(InvoiceLines.Quantity * ISNULL(InvoiceLines.UnitPrice, StockItems.UnitPrice)) AS PurchaseValue
@@ -30,13 +30,18 @@ BEGIN
 		INNER JOIN Warehouse.StockItems ON Warehouse.StockItems.StockItemID=InvoiceLines.StockItemID
 		GROUP BY InvoiceLines.InvoiceID
 	) /*Стоимость покупки. Один пользователь может совершить несколько покупок*/
-	SELECT TOP 1 @CustomerID = Customers.CustomerID--, Customers.CustomerName, PurchaseSum.PurchaseValue
+	SELECT TOP 1 @CustometID = Customers.CustomerID--, Customers.CustomerName, PurchaseSum.PurchaseValue
 	FROM Sales.Invoices 
 	INNER JOIN PurchaseSum ON PurchaseSum.InvoiceID=Invoices.InvoiceID
 	INNER JOIN Sales.Customers ON Customers.CustomerID=Invoices.CustomerID
 	ORDER BY PurchaseValue DESC
+
+	RETURN @CustometID
 END
 GO
+
+-- Пример вызова:
+SELECT Sales.GetBestBuyer()
 
 /*
 2) Написать хранимую процедуру с входящим параметром СustomerID, выводящую сумму покупки по этому клиенту.
@@ -56,19 +61,22 @@ IF OBJECT_ID('Sales.GetCustomerPurchasesTotal') IS NOT NULL
 GO
 
 CREATE PROCEDURE Sales.GetCustomerPurchasesTotal
-	@CustomerID INT,
-	@Value MONEY OUT
+	@CustomerID INT
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	SELECT @Value = SUM(InvoiceLines.Quantity * ISNULL(InvoiceLines.UnitPrice, StockItems.UnitPrice))
+	SELECT SUM(InvoiceLines.Quantity * ISNULL(InvoiceLines.UnitPrice, StockItems.UnitPrice)) AS Total
 	FROM Sales.InvoiceLines
 	INNER JOIN Warehouse.StockItems ON Warehouse.StockItems.StockItemID=InvoiceLines.StockItemID
 	INNER JOIN Sales.Invoices ON Invoices.InvoiceID=InvoiceLines.InvoiceID
 	WHERE Invoices.CustomerID=@CustomerID
 END
 GO
+
+-- Пример вызова:
+EXEC Sales.GetCustomerPurchasesTotal 834
+
 
 -- 3) Cозда5ть одинаковую функцию и хранимую процедуру, посмотреть в чем разница в производительности и почему
 
@@ -102,8 +110,8 @@ SET @MValue = Sales.GetCustomerPurchasesTotalUDF(@CustomerID);
 PRINT @MValue;
 
 
-EXEC Sales.GetCustomerPurchasesTotal @CustomerID, @MValue OUT;
-PRINT @MValue;
+EXEC Sales.GetCustomerPurchasesTotal @CustomerID;
+
 
 /*
 Планы выполнения ХП и UDF абсолютно одинаковые (файлы GetCustomerPurchasesTotal.sqlplan и GetCustomerPurchasesTotalUDF.sqlplan), поэтому разницы в производительности нет. 
@@ -145,3 +153,4 @@ CROSS APPLY(
 	SELECT InvoiceID, Total
 	FROM Sales.GetCustomerTop3Orders(Customers.CustomerID)
 	) AS TOP3
+
